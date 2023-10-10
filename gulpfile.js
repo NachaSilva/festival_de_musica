b82198c19667e40e0418fd8e62c9805f416da643
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2023-10-10 12:52:58 -0300

    uso de plumber, mixins y creación de layout. Además de configuración para media queries en snippet y uso de estas para finalizar header en medida rem

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done) {
   src('src/scss/**/*.scss') //Identificar el archivo de sass
-    .pipe(sass()) //compilar-ejecutar el archivo de sass
+  .pipe( plumber()) //da los errores más facil de leer 
+  .pipe(sass()) //compilar-ejecutar el archivo de sass
+    
     .pipe(dest('build/css')); //almacenarla en el disco duro
 
   done(); //Callback que avisa a gulp cuando llegamos al final
